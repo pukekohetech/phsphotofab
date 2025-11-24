9358f94149c7e90947cc165e85931af2ee86949d
pukekohetech <33167310+pukekohetech@users.noreply.github.com>
2025-11-24 17:07:50 +1300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -5,7 +5,7 @@ const SW_URL = new URL(self.location);
 const ROOT_PATH = SW_URL.pathname.replace(/service-worker\.js$/, '');
 
 // Bump this whenever you change core assets
-const CACHE_NAME = 'phs-stamper-v262';
+const CACHE_NAME = 'phs-stamper-v275';
 
 // Helper to build paths under the app root
 function atRoot(path) {
